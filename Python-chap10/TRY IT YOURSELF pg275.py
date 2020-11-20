d0bb1385c203cd50f27fcede9426714f6e9be1d4
Tatenda-Kumbula <58853224+Tatenda-Kumbula@users.noreply.github.com>
2020-11-20 02:54:30 +0200

    Add files via upload

---
+++ Python-chap10/TRY IT YOURSELF pg275.py
@@ -0,0 +1,86 @@
+#10-6
+
+try:
+    x = input("Give me a number: ")
+    x = int(x)
+
+    y = input("Give me another number: ")
+    y = int(y)
+except ValueError:
+    print("Sorry, I really needed a number.")
+else:
+    sum = x + y
+    print(f"The sum of {x} and {y} is {sum}.")
+
+
+ #10-7
+
+ print("Enter 'q' at any time to quit.\n")
+
+while True:
+    try:
+        x = input("\nGive me a number: ")
+        if x == 'q':
+            break
+
+        x = int(x)
+
+        y = input("Give me another number: ")
+        if y == 'q':
+            break
+
+        y = int(y)
+
+#10-8
+filenames = ['cats.txt', 'dogs.txt']
+
+for filename in filenames:
+    print(f"\nReading file: {filename}")
+    try:
+        with open(filename) as f:
+            contents = f.read()
+            print(contents)
+    except FileNotFoundError:
+        print("  Sorry, I can't find that file.")  
+
+  #10-9
+
+  filenames = ['cats.txt', 'dogs.txt']
+
+for filename in filenames:
+    
+    try:
+        with open(filename) as f:
+            contents = f.read()
+
+    except FileNotFoundError:
+        pass
+
+    else:
+        print(f"\nReading file: {filename}")
+        print(contents)
+
+    except ValueError:
+        print("Sorry, I really needed a number.")
+
+    else:
+        sum = x + y
+        print(f"The sum of {x} and {y} is {sum}.")
+
+#10-10
+
+def count_common_words(filename, word):
+    
+    try:
+        with open(filename, encoding='utf-8') as f:
+            contents = f.read()
+    except FileNotFoundError:
+        pass
+    else:
+        word_count = contents.lower().count(word)
+
+        msg = f"'{word}' appears in {filename} about {word_count} times."
+        print(msg)
+
+filename = 'alice.txt'
+count_common_words(filename, 'the')
---
+++ Python-chap10/TRY IT YOURSELF pg282.py
@@ -0,0 +1,55 @@
+
+
+#10-12
+import json
+
+try:
+    with open('favorite_number.json') as f:
+        number = json.load(f)
+except FileNotFoundError:
+    number = input("What's your favorite number? ")
+    with open('favorite_number.json', 'w') as f:
+        json.dump(number, f)
+    print("Thanks, I'll remember that.")
+else:
+    print(f"I know your favorite number! It's {number}.")
+
+
+#10-13
+
+import json
+
+def get_stored_username():
+    
+    filename = 'username.json'
+    try:
+        with open(filename) as f_obj:
+            username = json.load(f_obj)
+    except FileNotFoundError:
+        return None
+    else:
+        return username
+
+def get_new_username():
+    
+    username = input("What is your name? ")
+    filename = 'username.json'
+    with open(filename, 'w') as f_obj:
+        json.dump(username, f_obj)
+    return username
+
+def greet_user():
+    
+    username = get_stored_username()
+    if username:
+        correct = input(f"Are you {username}? (y/n) ")
+        if correct == 'y':
+            print(f"Welcome back, {username}!")
+        else:
+            username = get_new_username()
+            print(f"We'll remember you when you come back, {username}!")
+    else:
+        username = get_new_username()
+        print(f"We'll remember you when you come back, {username}!")
+
+greet_user()

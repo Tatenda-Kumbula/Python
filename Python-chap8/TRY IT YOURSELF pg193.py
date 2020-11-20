c84ccc43974b992adbcfe526034cf1d05bbf5d02
Tatenda-Kumbula <58853224+Tatenda-Kumbula@users.noreply.github.com>
2020-11-20 02:31:21 +0200

    Add files via upload

---
+++ Python-chap8/TRY IT YOURSELF pg193.py
@@ -0,0 +1,18 @@
+#8-1. Message:
+
+def display_message():
+	print("We are learning about Arguments and Parameters!")
+display_message()
+
+
+#8-2. Favorite Book
+
+
+def favorite_book():
+	print("My favorite book is Sophia Town!")
+favorite_book()
+
+
+
+
+ 

a5f267938d7e4dd4040f25c4bfeb1dbc61aba627
Tatenda-Kumbula <58853224+Tatenda-Kumbula@users.noreply.github.com>
2020-11-20 02:13:51 +0200

    Add files via upload

---
+++ Python-chap5/TRY IT YOURSELF pg139.py
@@ -0,0 +1,95 @@
+alien_color='red'
+if alien_color =='green':
+	print('+5')
+else:
+	print('you have got no points')
+
+
+alien_color='green'
+if alien_color =="green":
+	print('+5')
+else:
+	print('you get points')
+
+#5-4. Alien Colors
+alien_color='green'
+if alien_color=='green': 
+  print ('Player earned 5 points')
+else:
+	print('Player earned 10 points')
+
+alien_color='red'
+if alien_color=='green': 
+  print ('Player earned 5 points')
+else:
+	print('Player earned 10 points')
+
+#5.5 Alien Colors #3:
+alien_color='green'
+if alien_color=='green': 
+  print ('Player earned 5 points')
+elif alien_color=='yellow':
+	print('Player earned 10 points')
+else:
+ 	print('Player earned 15  points' )
+
+
+alien_color='red'
+if alien_color=='green': 
+  print ('Player earned 5 points')
+elif alien_color=='yellow':
+	print('Player earned 10 points')
+else:
+ 	print('Player earned 15  points' )
+
+
+alien_color='yellow'
+if alien_color=='green': 
+  print ('Player earned 5 points')
+elif alien_color=='yellow':
+	print('Player earned 10 points')
+else:
+ 	print('Player earned 15  points' )
+
+
+
+#5-6. Stages of Life:
+
+age=65
+if age < 2:
+	print('Person is a baby')
+elif age >= 2 and age < 4:
+	print('Person is a toddler')
+elif age >= 4 and age < 13:
+	print('Person is a kid')
+elif age >= 13 and age < 20:
+	print('Person is a teenager')
+elif age >= 20 and age <65:
+	print('Person is an adult')
+else:
+	print('Person is an elder')
+
+
+
+#5-7. Favorite Fruit
+
+favorite_fruits=['Apple','Peach','Strawberry','Banana','Pineapple']
+
+if 'Apple' in favorite_fruits:
+	print("You really like Apple.")
+
+if "Peach" in favorite_fruits:
+	print("You really like Peach.")
+
+if  "Strawberry" in favorite_fruits:              
+	print("You really like Strawberry.")
+
+
+if  "Banana" in favorite_fruits:              
+	print("You really like Banana.")
+
+
+if  "Pineapple" in favorite_fruits:              
+	print("You really like Pineapple.")	
+
+
---
+++ Python-chap5/TRY IT YOURSELF pg144.py
@@ -0,0 +1,48 @@
+#5-8
+
+usernames = ['tatenda', 'willie', 'admin', 'tom', 'jerry']
+
+for username in usernames:
+    if username == 'admin':
+        print("Hello admin, would you like to see a status report?")
+    else:
+        print(f"Hello {username}, thank you for loggin in again!")
+
+
+#5-9
+
+usernames = []
+
+if usernames:
+    for username in usernames:
+        if username == 'admin':
+            print("Hello admin, would you like to see a status report?")
+        else:
+            print(f"Hello {username}, thank you for loggin in again!")
+else:
+    print("We need to find some users!")
+
+ #5-10
+current_users = ['tatenda', 'willie', 'admin', 'tom', 'jerry']
+new_users = ['sarah', 'Willie', 'PHIL', 'tom', 'jerry']
+
+current_users_lower = [user.lower() for user in current_users]
+
+for new_user in new_users:
+    if new_user.lower() in current_users_lower:
+        print(f"Sorry {new_user}, that name is taken.")
+    else:
+        print(f"Great, {new_user} is still available.")
+
+#5-11
+numbers = list(range(1,10))
+
+for number in numbers:
+    if number == 1:
+        print("1st")
+    elif number == 2:
+        print("2nd")
+    elif number == 3:
+        print("3rd")
+    else:
+        print(f"{number}th")
